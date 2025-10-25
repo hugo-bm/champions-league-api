d0e6afe97c1918b4c1924a32e5f2d57dde6314ca
Hugo Batalha Moreno <hugomb123@gmail.com>
2025-10-25 21:39:37 +0000

    🐛 fix(middleware): correct errorHandler signature for Express recognition
    Added the `next` parameter to the errorHandler signature.
    Although "express-async-errors" handles async exceptions automatically, Express requires a four-parameter signature to properly identify a custom error handler.

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,12 +1,12 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import HttpError from '../errors/HttpError';
 
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function errorHandler (err: any, req: Request, res: Response){
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+export function errorHandler (err: any, req: Request, res: Response, next: NextFunction){
     if (err instanceof HttpError) {
         return res.status(err.statusCode).json({error: err.message});
     }
     console.error(err);
-    return res.status(500).json({error: "Internal server error"})
+    return res.status(500).json({error: "Internal server error"});
 }
